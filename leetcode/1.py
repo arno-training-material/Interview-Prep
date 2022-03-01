ca9401f0c16291e1267ddab0d1680034841ffff6
ArnoStrouwen <arno.strouwen@telenet.be>
2022-03-01 23:33:39 +0100

    leetcode 1

---
+++ leetcode/1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict_nums = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in dict_nums:
+                return (i,dict_nums[complement])
+            dict_nums[nums[i]] = i
